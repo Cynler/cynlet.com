70b1d28c8b0397969c13371469ee68e83524d52a
Cynler <lucynthi@gmail.com>
2020-04-06 18:08:41 +0800

    about page fix | package update

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('img/**.svg');
+  eleventyConfig.addPassthroughCopy('_data/**.json');
 
   return {
+    templateFormats: [
+      "md",
+      "ejs",
+      "css",
+      "svg",
+      "png",
+      "json",
+      "mustache",
+    ],
     passthroughFileCopy: true
   }
 }
