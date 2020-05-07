afe07fe1f2eb83f89464498f36c4aa48e3de2fbe
Cynler <lucynthi@gmail.com>
2020-05-07 19:49:35 +0800

    njk componentized | SASS implemented | CSS reworked not yet done

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+module.exports = function(eleventyConfig) {  
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('img/**.svg');
   eleventyConfig.addPassthroughCopy('_data/**.json');
